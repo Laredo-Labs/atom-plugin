bfcd1b6a79454b7cfdda7249614ab8df0dfb2eb9
Dan Lord <blahblahasdf@gmail.com>
2022-04-12 12:06:30 -0700

    Update atom plugin to talk to demo prompt generation service.

--- src/providers/remote/types.ts
+++ src/providers/remote/types.ts
@@ -1,18 +1,31 @@
 'use babel'
 
 export type Request = {
-  prefix: string;
-  filename: string;
-  beforeContext?: string | undefined;
-  afterContext?: string | undefined;
+  repo_path: string;
+  server_url?: string | undefined
+  commit_msg?: string | undefined;
+  path?: string | undefined;
+  current_line?: CurrentLine | undefined;
+  context_lines?: Line[] | undefined;
+  path_filters?: String[] | undefined; 
 }
 
 export type Response = {
-  prefix: string;
   responses: ResponseEntry[];
 }
 
 export type ResponseEntry = {
   confidence: number;
   value: string;
+}
+
+export type Line = {
+  line_num: number;
+  source: string;
+}
+
+export type CurrentLine = {
+  line_num: number;
+  source: string;
+  caret: number;
 }
--- src/providers/remote/utils.ts
+++ src/providers/remote/utils.ts
@@ -4,14 +4,13 @@ import * as types from './types';
 
 const axios = require('axios').default;
 const service = axios.create({
-  baseURL: "https://random-word-api.herokuapp.com",
+  baseURL: "http://127.0.0.1:5000",
   timeout: 5000
 });
 
 export async function autocompleteHttp(params: types.Request): Promise<types.Response> {
-  const response = await service.get('/word', {params: {number:  10, swear: 0}});
+  const response = await service.post('/generate', params);
   return {
-    prefix: params.prefix,
     responses: response.data?.map((entry: any) => {
       return {
         confidence: -1.0,
